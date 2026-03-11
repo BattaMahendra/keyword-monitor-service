ef3150627342399bafe251569e5ef5df5dcc2c63
BATTA_MAHENDRA <battamahendra26@gmail.com>
2026-03-11 09:16:42 +0530

    Adding test level configs

--- src/test/java/com/mahi/KeywordCheckHubApplicationTests.java
+++ src/test/java/com/mahi/KeywordCheckHubApplicationTests.java
@@ -1,10 +1,22 @@
 package com.mahi;
 
+import com.mahi.notification.EmailNotificationService;
+import com.mahi.notification.TelegramNotificationService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
+
 class KeywordCheckHubApplicationTests {
+
+	@MockBean
+	private EmailNotificationService emailNotificationService;
+
+	@MockBean
+	private TelegramNotificationService telegramNotificationService;
 
 	@Test
 	void contextLoads() {
